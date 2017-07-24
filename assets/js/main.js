f8041d5076dd18bede375ffd302bb0330107565d
Tiago Vasconcelos <tisvasconcelos+tisvasconcelos@gmail.com>
2017-07-24 19:51:05 -0300

    Adiciona pagina e css inicial da guideline

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+var examples = Array.prototype.slice.call(document.querySelectorAll('.example'));
+examples.forEach(example => {
+  code = example.nextElementSibling;
+  if(code) {
+    var html = escapeHtml(example.innerHTML);
+    code.querySelector('code').innerHTML = html;
+  }
+});
